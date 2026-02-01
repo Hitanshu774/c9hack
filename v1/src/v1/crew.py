5e30dfb4da11285eed1867828ff9a87e2d336f2b
Hitanshu774 <hitanshupatil143@gmail.com>
2026-02-01 15:51:19 +0530

    starting to create custom database 1

--- v1/src/v1/crew.py
+++ v1/src/v1/crew.py
@@ -13,12 +13,7 @@ class V1():
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
+#####################################################################
     @agent
     def researcher(self) -> Agent:
         return Agent(
@@ -33,9 +28,7 @@ class V1():
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+######################################################################
     @task
     def research_task(self) -> Task:
         return Task(
